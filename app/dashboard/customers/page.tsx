74ac317eedad758207357275b6a1191e84870246
Walia <itswalia20@gmail.com>
2024-10-15 02:10:29 +0530

    This new updated till chapter 6

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Customers = () => {
+  return (
+    <div>
+      <p>This is Customer Page </p>
+    </div>
+  )
+}
+
+export default Customers
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Invoices = () => {
+  return (
+    <div>
+        <p>This is Invoices Page</p>
+    </div>
+  )
+}
+
+export default Invoices
